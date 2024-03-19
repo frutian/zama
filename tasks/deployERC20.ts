cfb1884e543205037b7878fbb8e4ad4270fe11b6
frutian <37641680+frutian@users.noreply.github.com>
2024-03-19 16:14:27 +0300

    mass

--- tasks/deployERC20.ts
+++ tasks/deployERC20.ts
@@ -4,7 +4,9 @@ import type { TaskArguments } from "hardhat/types";
 task("task:deployERC20").setAction(async function (taskArguments: TaskArguments, { ethers }) {
   const signers = await ethers.getSigners();
   const erc20Factory = await ethers.getContractFactory("ConfidentialERC20");
-  const encryptedERC20 = await erc20Factory.connect(signers[0]).deploy("Naraggara", "NARA"); // City of Zama's battle);
-  await encryptedERC20.waitForDeployment();
-  console.log("EncryptedERC20 deployed to: ", await encryptedERC20.getAddress());
+  for (let i = 0; i < 2; i++) {
+    const encryptedERC20 = await erc20Factory.connect(signers[0]).deploy("Naraggara", "NARA"); // City of Zama's battle);
+    await encryptedERC20.waitForDeployment();
+    console.log("EncryptedERC20 deployed to: ", await encryptedERC20.getAddress());
+  }
 });
